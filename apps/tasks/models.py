c8c1ae3d2418f12a2807775c63ad4dd0fa3edf87
Dmitriy Fishman <fishman.code@gmail.com>
2023-02-25 21:02:11 +0200

    Created a 'task' model

--- apps/tasks/models.py
+++ apps/tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Task(models.Model):
+    class Status(models.IntegerChoices):
+        BACKLOG = 1, _('Backlog')
+        IN_PROGRESS = 2, _('In progress')
+        DONE = 3, _('Done')
+
+    name = models.CharField(max_length=256)
+    status = models.SmallIntegerField(default=Status.BACKLOG, choices=Status.choices)
+    due_date = models.DateField(null=True)
+
+    class Meta:
+        db_table = "task"
