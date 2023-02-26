9c3263009b06c50dbfb4597a415ee74efe4f21f4
Dmitriy Fishman <fishman.code@gmail.com>
2023-02-26 17:23:35 +0200

    Installed 'djangorestframework-simplejwt' package

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -150,4 +150,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework_simplejwt.authentication.JWTStatelessUserAuthentication',)
 }
--- todo_list/urls.py
+++ todo_list/urls.py
@@ -17,6 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from apps.tasks.views import TaskViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 router.register(r'tasks', TaskViewSet)
@@ -24,4 +28,6 @@ router.register(r'tasks', TaskViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
