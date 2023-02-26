c0bcb8239cc9cfefb264ca63a2cbd000a7ad6265
Dmitriy Fishman <fishman.code@gmail.com>
2023-02-26 17:32:32 +0200

    Only authenticated users can access the task route

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 from .models import Task
 from .serializers import TaskSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     filterset_fields = ('status',)
