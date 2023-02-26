14e88986c238c7c7727ac248d326d01dcf0d5db8
Dmitriy Fishman <fishman.code@gmail.com>
2023-02-26 14:42:08 +0200

    Installed and enabled the 'django_filters' package

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -59,6 +59,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'django_filters',
     'apps.tasks',
 ]
 
@@ -148,4 +149,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
