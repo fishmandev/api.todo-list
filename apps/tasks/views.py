2ed4ab1a814a479cec0ff87a25a385f1a4232eae
Dmitriy Fishman <fishman.code@gmail.com>
2023-02-26 14:43:07 +0200

    Added the ability to filter tasks by status

--- apps/tasks/views.py
+++ apps/tasks/views.py
@@ -6,3 +6,4 @@ from .serializers import TaskSerializer
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    filterset_fields = ('status',)
